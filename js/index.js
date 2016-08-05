f52cc715ae37b95dbe056952519c7f233652bdbe
谭翔 <510754787@qq.com>
2016-08-05 19:29:21 +0800

    second

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 $(function(){
-    $('#dowebok').fullpage({
-    	'navigation': true, 
-		'navigationPosition': 'right', 
-		'navigationTooltips': ['登录百度舆情', '领先：数据收集与处理', '全面：舆情分析逻辑与架构', '专业：专业数据可视化'],
-    });
-});
-/*var st1=$(".sectio:first").offsetTop;
-if ($(window)<=st1) {
-	$(".img4").addClass(".active");
-};
-$(".xyy").click(function(){
-	$.fn.fullpage.moveSectionDown();
-});*/
+    //联系我们点击事件开始
+    $(".header .header-nav .right ul li:nth-child(7)").on("click",function(){
+        $(".header .header-nav .right ul li:nth-child(7) .lianXi-xxk").toggleClass("active");
+    })
+
+    //更换验证码点击事件开始
+    var verifyImg=$("#main .section-1 .center .login-box .verify .verify-center img");
+    var verify=$("#main .section-1 .center .login-box .verify .verify-right");
+    var ac=$("#main .section-1 .center .login-box .verify .verify-center .active");
+    verify.on("click",function(){
+        ac=$("#main .section-1 .center .login-box .verify .verify-center .active");
+        verifyImg.removeClass("active");
+        var next=ac.next();
+        if(next.length){
+            ac.next().addClass("active");
+        }else{
+            verifyImg.first().addClass('active');
+        }
+    })
+})
